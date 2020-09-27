3cb19a88f35ea05a24fc2c9c2844d487cfd86592
Balaji Kummari <balajievlo@gmail.com>
2020-09-27 20:05:20 +0530

    build fix

--- components/post-body.js
+++ components/post-body.js
@@ -1,7 +1,6 @@
 import { Container } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
 import Parser from 'html-react-parser';
-import {Link, RichText, Date} from 'prismic-reactjs';
 
 
 export default function PostBody({ content }) {
